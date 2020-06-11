f1fc8db2e49f9ebc65abb4e7a89543b3cd2416eb
Yeqing98 <1835132202@qq.com>
2020-06-11 16:14:04 +0800

    学习import引入模块以及配置

---
+++ 13-import/dist/page.js
@@ -0,0 +1,44 @@
+define("components", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Header = /** @class */ (function () {
+        function Header() {
+            var ele = document.createElement('div');
+            ele.innerHTML = "This is Header";
+            document.body.appendChild(ele);
+        }
+        return Header;
+    }());
+    exports.Header = Header;
+    var Content = /** @class */ (function () {
+        function Content() {
+            var ele = document.createElement('div');
+            ele.innerHTML = "This is Content";
+            document.body.appendChild(ele);
+        }
+        return Content;
+    }());
+    exports.Content = Content;
+    var Footer = /** @class */ (function () {
+        function Footer() {
+            var ele = document.createElement('div');
+            ele.innerHTML = "This is Footer";
+            document.body.appendChild(ele);
+        }
+        return Footer;
+    }());
+    exports.Footer = Footer;
+});
+define("page", ["require", "exports", "components"], function (require, exports, components_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Page = /** @class */ (function () {
+        function Page() {
+            new components_1.Header();
+            new components_1.Content();
+            new components_1.Footer();
+        }
+        return Page;
+    }());
+    exports.default = Page;
+});
---
+++ 13-import/src/components.ts
@@ -0,0 +1,23 @@
+export class Header {
+    constructor() {
+        const ele = document.createElement('div');
+        ele.innerHTML = "This is Header";
+        document.body.appendChild(ele);
+    }
+}
+
+export class Content {
+    constructor() {
+        const ele = document.createElement('div');
+        ele.innerHTML = "This is Content";
+        document.body.appendChild(ele);
+    }
+}
+
+export class Footer {
+    constructor() {
+        const ele = document.createElement('div');
+        ele.innerHTML = "This is Footer";
+        document.body.appendChild(ele);
+    }
+}
